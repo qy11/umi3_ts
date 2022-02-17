c28ce84662d7ffe1c455685c343929e274886f35
qy <yuqiao565@163.com>
2022-02-17 14:41:40 +0800

    feat: 上传demo

---
+++ src/pages/About/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const index = () => {
+  return (
+    <div>
+      <h1>鲜衣怒马</h1>
+      <p>早晨你来过留下过弥漫着樱花香</p>
+      <img width="1000" src={require('@/assets/order.png')} alt="" />
+    </div>
+  );
+};
+
+export default index;
